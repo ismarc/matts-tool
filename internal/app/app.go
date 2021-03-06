bf3cfdda8cfa64a0491e97e2ae44eaf9812cdbfd
Matt Brace <ismarc31@gmail.com>
2021-03-06 00:47:15 -0600

    Handle base and relative paths for includes properly

--- internal/app/app.go
+++ internal/app/app.go
@@ -5,11 +5,16 @@ import (
 	"io"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 
 	"gopkg.in/yaml.v3"
 )
 
-func loadFile(inputFile string) ([]byte, error) {
+type policyLoader struct {
+	filePath string
+}
+
+func (p policyLoader) loadFile(inputFile string) ([]byte, error) {
 	var inFile io.Reader
 	var err error
 
@@ -17,7 +22,7 @@ func loadFile(inputFile string) ([]byte, error) {
 	case "-":
 		inFile = os.Stdin
 	default:
-		inFile, err = os.Open(inputFile)
+		inFile, err = os.Open(filepath.Join(p.filePath, inputFile))
 		if err != nil {
 			panic(err)
 		}
@@ -26,19 +31,23 @@ func loadFile(inputFile string) ([]byte, error) {
 	return ioutil.ReadAll(inFile)
 }
 
-func loadPolicyYaml(inData []byte) (result yaml.Node, err error) {
-	err = yaml.Unmarshal(inData, &IncludeProcessor{&result})
+func (p policyLoader) loadPolicyYaml(inData []byte) (result yaml.Node, err error) {
+	processor := IncludeProcessor{&result, p}
+	// err = yaml.Unmarshal(inData, &IncludeProcessor{&result, "foo"})
+	err = yaml.Unmarshal(inData, &processor)
 	return
 }
 
 // Run is the main entrypoint for the policy-handler
 func Run(inputPolicyFile string) {
-	data, err := loadFile(inputPolicyFile)
+	loader := policyLoader{filepath.Dir(inputPolicyFile)}
+
+	data, err := loader.loadFile(filepath.Base(inputPolicyFile))
 	if err != nil {
 		panic(err.Error())
 	}
 
-	result, err := loadPolicyYaml(data)
+	result, err := loader.loadPolicyYaml(data)
 	out, err := yaml.Marshal(result)
 	fmt.Printf("%+v\n", string(out))
 }
--- internal/app/include_processor.go
+++ internal/app/include_processor.go
@@ -8,21 +8,23 @@ import (
 
 type Fragment struct {
 	content *yaml.Node
+	loader  policyLoader
 }
 
 type IncludeProcessor struct {
 	target interface{}
+	loader policyLoader
 }
 
 func (fragment *Fragment) UnmarshalYAML(value *yaml.Node) error {
 	var err error
 
-	fragment.content, err = processIncludes(value)
+	fragment.content, err = processIncludes(value, fragment.loader)
 	return err
 }
 
 func (include *IncludeProcessor) UnmarshalYAML(value *yaml.Node) error {
-	processed, err := processIncludes(value)
+	processed, err := processIncludes(value, include.loader)
 
 	if err != nil {
 		return err
@@ -31,7 +33,7 @@ func (include *IncludeProcessor) UnmarshalYAML(value *yaml.Node) error {
 	return processed.Decode(include.target)
 }
 
-func processIncludes(node *yaml.Node) (*yaml.Node, error) {
+func processIncludes(node *yaml.Node, loader policyLoader) (*yaml.Node, error) {
 	// Skip `replace` nodes because they aren't v5 compatible
 	if node.Value == "replace" {
 		node.Value = ""
@@ -42,12 +44,12 @@ func processIncludes(node *yaml.Node) (*yaml.Node, error) {
 			return nil, fmt.Errorf("!include on a non-scalar node")
 		}
 
-		data, err := loadFile(node.Value)
+		data, err := loader.loadFile(node.Value)
 		if err != nil {
 			return nil, err
 		}
 
-		var fragment Fragment
+		fragment := Fragment{loader: loader}
 		err = yaml.Unmarshal(data, &fragment)
 		return fragment.content, err
 	}
@@ -55,7 +57,7 @@ func processIncludes(node *yaml.Node) (*yaml.Node, error) {
 	if node.Kind == yaml.SequenceNode || node.Kind == yaml.MappingNode {
 		var err error
 		for i := range node.Content {
-			node.Content[i], err = processIncludes(node.Content[i])
+			node.Content[i], err = processIncludes(node.Content[i], loader)
 			if err != nil {
 				return nil, err
 			}
